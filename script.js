3a48ca54545360d3f7af578c6747a5709ac7381b
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-04-07 13:33:47 -0400

    updated with button tag ...

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 async function getInfo() {
   try {
     const results = await axios.get(`https://opentdb.com/api.php?amount=20&category=12&difficulty=medium&type=boolean`);
@@ -16,6 +18,7 @@ getInfo();
 // For each question, user clicks true or false button/block
 
 
+
 // If answer is 'true', user is alerted 'You Rock!'
 function trueAnswer() {
   document.getElementById('question').innerHTML = 'YOU ROCK !!!';
@@ -27,6 +30,24 @@ function falseAnswer() {
   document.getElementById('question').innerHTML = 'YOU SUCK !!!';
 }
 
+
+//Write a method that receives the array of 'results' objects as a paramter
+
+
+// Call the method
+
+
+// When user clicks true or false button, pass teh answer to the api and fetch the information ?? AKA create a function to append to the DOM/buttons
+
+
+// if/else or switch case for true or false? 
+
+
+// .get request ???
+
+
+
+
 // After 20 questions answered, If user answers more than 10 questions correct, alert user 'You Owned It! You're a F*ckin Rockstar!"
 
 
@@ -34,9 +55,9 @@ function falseAnswer() {
 
 
 
-// NOW ADD DATA TO THE DOM ? ...
+// ADD DATA TO THE DOM ? ...
 
 
 // EVENT LISTENER TAKES PLACE IN THE DOM -- button click
 
-
+// Need autorefresh after question is answered?
