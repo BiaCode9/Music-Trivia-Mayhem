33688645afde181a3d3bb072abec19c7f86928eb
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-04-07 15:33:44 -0400

    attempt to compare correct answer to string attached to button

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-
+const question = document.querySelector('#question') //traversing the DOM
 
 async function getInfo() {
   try {
     const results = await axios.get(`https://opentdb.com/api.php?amount=20&category=12&difficulty=medium&type=boolean`);
-    console.log(results);
+    console.log(results.data.results);
+    const response = results.data.results
+
+    // response.forEach(item => {
+    //   const triviaQuestion = item.question
+    //   console.log(triviaQuestion)
+    //   question.innerHTML = triviaQuestion
+    //   const correctAnswer = item.correct_answer
+    //   console.log(correctAnswer)
+    // })
+
+
+    const triviaQuestion = response[0].question
+    question.innerHTML = triviaQuestion
+
+    // see if the boolean value are tru compared to each button you are comparing
+
+    // then have an incrementer // then increment to next question 
+
+    const questionNum = 0
+
   } catch (error) {
     console.log(error);
   }
 }
 
-getInfo();
+
+let button = document.querySelector("button");
+button.addEventListener("click", event => {
+  if (event.button.innerHtml === 'correct_answer') {
+    console.log("You Rock!");
+  } else if (event.button.innerHtml === 'incorrect_answers')
+    console.log("You Suck!");
+
+  // increment through the array for every question 
+  // check to see if the answer is the same for what the button given is giving back === make the comparison with if else 
+
+  // nextQuestion() {
+
+
+  // }
+
+  getInfo();
 
 // PSEUDOCODE:
 
@@ -20,15 +56,15 @@ getInfo();
 
 
 // If answer is 'true', user is alerted 'You Rock!'
-function trueAnswer() {
-  document.getElementById('question').innerHTML = 'YOU ROCK !!!';
-}
+// function trueAnswer() {
+//   document.getElementById('question').innerHTML = 'YOU ROCK !!!';
+// }
 
 // Else, answer is wrong 'false' , user is alerted 'You Suck!'
 
-function falseAnswer() {
-  document.getElementById('question').innerHTML = 'YOU SUCK !!!';
-}
+// function falseAnswer() {
+//   document.getElementById('question').innerHTML = 'YOU SUCK !!!';
+// }
 
 
 //Write a method that receives the array of 'results' objects as a paramter
