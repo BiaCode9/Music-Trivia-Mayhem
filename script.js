4a4184195463e6a3e34eb442a06791324c3e9a21
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-04-08 13:45:37 -0400

    flex-box attempt

--- script.js
+++ script.js
@@ -51,15 +51,6 @@ async function getInfo() {
     }
 
 
-    // const
-    // if (answer === correctAnswer) {
-
-    // } else {
-
-    // }
-
-
-
     // Event Listeners - check and pass through to see if right or wrong
     trueButton.addEventListener('click', function () {
       check('True');
@@ -76,73 +67,3 @@ async function getInfo() {
 getInfo();
 
 
-// button.addEventListener("click", () => {
-//   if (event.button.innerHtml === 'correct_answer') {
-//     console.log("You Rock!");
-//   } else if (event.button.innerHtml === 'incorrect_answers') { 
-//     console.log("You Suck!");
-//   }
-
-// }
-
-
-
-  // increment through the array for every question 
-  // check to see if the answer is the same for what the button given is giving back === make the comparison with if else 
-
-  // nextQuestion() {
-
-
-  // }
-
-
-  // PSEUDOCODE:
-
-  // A trivia game where user goes through 20 music trivia questions
-
-  // For each question, user clicks true or false button/block
-
-
-
-  // If answer is 'true', user is alerted 'You Rock!'
-  // function trueAnswer() {
-  //   document.getElementById('question').innerHTML = 'YOU ROCK !!!';
-  // }
-
-  // Else, answer is wrong 'false' , user is alerted 'You Suck!'
-
-  // function falseAnswer() {
-  //   document.getElementById('question').innerHTML = 'YOU SUCK !!!';
-  // }
-
-
-  //Write a method that receives the array of 'results' objects as a paramter
-
-
-  // Call the method
-
-
-  // When user clicks true or false button, pass teh answer to the api and fetch the information ?? AKA create a function to append to the DOM/buttons
-
-
-  // if/else or switch case for true or false? 
-
-
-  // .get request ???
-
-
-
-
-  // After 20 questions answered, If user answers more than 10 questions correct, alert user 'You Owned It! You're a F*ckin Rockstar!"
-
-
-  //  If not, alert "You just don't have what it takes! Try again!"
-
-
-
-  // ADD DATA TO THE DOM ? ...
-
-
-  // EVENT LISTENER TAKES PLACE IN THE DOM -- button click
-
-  // Need autorefresh after question is answered?
